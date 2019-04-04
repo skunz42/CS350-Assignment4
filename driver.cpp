91823712c8fb4f7c19ea27ac0968e36d4a81c06e
Sean Kunz <skunz598@gmail.com>
2019-04-04 13:54:41 -0400

    Added error checking

--- driver.cpp
+++ driver.cpp
@@ -11,13 +11,24 @@ void createThreads(ReadWrite *rw, int numReaders, int numWriters) {
 }
 
 int main(int argc, char **argv) {
-	/*#######################
-	  Add in error checking
-	#######################*/
 	int numRandom = atoi(argv[1]);
 	int numReaders = atoi(argv[2]);
 	int numWriters = atoi(argv[3]);
+	if (numRandom < 1 || numRandom > 1000) {
+		cout << "Input a number in the range 1-1000" << endl;
+		return 1;
+	}
+	if (numReaders < 1 || numReaders > 9) {
+		cout << "Input a number in the range 1-9" << endl;
+		return 1;
+	}
+	if (numWriters < 1 || numWriters > 9) {
+		cout << "Input a number in the range 1-9" << endl;
+		return 1;
+	}
+
 	ReadWrite *rw = new ReadWrite (numReaders, numWriters, numRandom);
 	createThreads(rw, numReaders, numWriters);
+	delete(rw);
 	return 0;
 }
